9d58592c82eae25c50c3af144e42b2b423c774fd
awp123938 <grudinin2@gmail.com>
2022-11-10 20:52:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int value1 = 300;
+        int value2 = 120;
+        System.out.println(value1 - value2);
+
+
+	// write your code here
+    }
+}
